8b3f9519e88c5a6922ac699585e22330bee6ccd8
Remo Samuel Paul <remosam42@gmail.com>
2014-04-08 16:34:09 +0300

    [Emily/Remo] data submitter can uplaod attachments in any state

--- questionnaire/static/js/preview.js
+++ questionnaire/static/js/preview.js
@@ -16,9 +16,6 @@ function disableInputFields(status) {
         {$save_form_content.show();}
 
     $('.add-more').prop('disabled', status);
-    var $upload_form_content = $('.upload_form_content');
-    if (status)
-        {$upload_form_content.hide();}
-    else
-        {$upload_form_content.show();}
+
+
 }
