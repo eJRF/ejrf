29698a91542df5c3ea2c833c00d82893794fe0c7
Hugo Firth <hugo.firth@gmail.com>
2014-10-17 15:14:29 +0300

    got rid of js test that was suppose to fail

--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,4 @@
 describe("crappy test", function() {
-    it("should fail", function() {
-        expect(1).toEqual(2);
-    });
-
     it("should pass", function() {
         expect(1).toEqual(1);
     });
