5d91b5f85746d45cd293f02833bf4934d9b1859d
Hugo Firth <hugo.firth@gmail.com>
2014-10-20 11:55:33 +0300

    Revert 1bee73e..ba82317
    This rolls back to commit 1bee73ed8dc6dd0bc7c0d4d95cf3859f0fe036e4.

--- eJRF/snap-ci/snap-settings.py
+++ eJRF/snap-ci/snap-settings.py
@@ -3,10 +3,11 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "app_test",
-        "USER": "go",
-        "PASSWORD": "go",
-        "HOST": "localhost",
+        "NAME": "%s" % os.environ['SNAP_DB_PG_NAME'],
+        "USER": "%s" % os.environ['SNAP_DB_PG_USER'],
+        "PASSWORD": "%s" % os.environ['SNAP_DB_PG_PASSWORD'],
+        "HOST": "%s" % os.environ['SNAP_DB_PG_HOST'],
+        "PORT": "%s" % os.environ['SNAP_DB_PG_PORT']
     }
 }
 
