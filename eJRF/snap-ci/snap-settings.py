60aa20efa26fdda32ae447b09b3c51290f3c2eb0
Hugo Firth <hugo.firth@gmail.com>
2014-10-20 11:47:30 +0300

    fixed typo with extra comma

--- eJRF/snap-ci/snap-settings.py
+++ eJRF/snap-ci/snap-settings.py
@@ -3,7 +3,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "%s" % os.environ['SNAP_DB_PG_NAME'],
         "USER": "%s" % os.environ['SNAP_DB_PG_USER'],
-        "PASSWORD": "%s" % os.environ['SNAP_DB_PG_PASSWORD'],,
+        "PASSWORD": "%s" % os.environ['SNAP_DB_PG_PASSWORD'],
         "HOST": "%s" % os.environ['SNAP_DB_PG_HOST'],
         "PORT": "%s" % os.environ['SNAP_DB_PG_PORT']
     }
