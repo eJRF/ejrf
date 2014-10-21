e38fc81d2db7b172a44230e602a118ea7da09b62
Hugo Firth <hugo.firth@gmail.com>
2014-10-21 09:25:21 +0300

    added some logging to ft.sh so we can see how far snap makes it

--- eJRF/snap-ci/ft.sh
+++ eJRF/snap-ci/ft.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "starting functional tests"
 source ../ejrf_env/bin/activate
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
+echo "setup for tests done"
 ./manage.py harvest --tag=-WIP
+echo "functional test complete"
