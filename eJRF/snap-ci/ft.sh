5458c193fea88b73ced5b9d2e626a4273f364f17
Hugo Firth <hugo.firth@gmail.com>
2014-10-23 09:39:13 +0300

    changed directory it's looking for the virtual environment in

--- eJRF/snap-ci/ft.sh
+++ eJRF/snap-ci/ft.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "starting functional tests"
-source ../ejrf_env/bin/activate
+source ejrf_env/bin/activate
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
 echo "setup for tests done"
 ./manage.py harvest --tag=-WIP
