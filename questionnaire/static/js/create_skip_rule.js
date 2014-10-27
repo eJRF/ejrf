50ca106b7adc191abeb94cf7b292bc12df44e4a7
Hugo Firth <hugo.firth@gmail.com>
2014-10-27 12:02:47 +0300

    hooking the new end point up to the front end

--- questionnaire/static/js/create_skip_rule.js
+++ questionnaire/static/js/create_skip_rule.js
@@ -1,4 +1,6 @@
 /** @jsx React.DOM */
+var skipRules = skipRules || {};
+skipRules.subsection = "";
 
 var Responses = React.createClass({
     render: function() {
@@ -28,9 +30,11 @@ var Question = React.createClass({
         this.props.selectQuestion(v);
     },
     render: function() {
-        var options = this.props.questions.map(function(q) {
+        var options = this.props.questions
+                .filter(this.props.filterFunction)
+                .map(function(q) {
             return (
-                <option value={q.id}>{q.id}</option>
+                <option value={q.pk}>{q.fields.text}</option>
             );
         });
         return (
@@ -45,30 +49,44 @@ var Question = React.createClass({
 });
 
 var AddSkipRule = React.createClass({
-    dummyFunction: function(a) { return; },
+    dummyFunction: function(a) { return true; },
     selectQuestion: function(question) {
         var selectedQuestion = this.state.questions.filter(function(q) {
-            return q.id == question;
+            return q.pk == question;
         })[0];
         this.setState({questions:this.state.questions, selectedQuestion: selectedQuestion});
     },
     getInitialState: function() {
-        return {questions:[ {id: 10, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]},
-                            {id: 20, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]},
-                            {id: 30, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]}],
-               selectedQuestion: {id: null, responses: []}};
+        return {
+            questions: [],
+            selectedQuestion: {pk: null, responses: []}};
     },
+    filterQuestions: function(q) { return q.fields.answer_type == "MultiChoice"; },
     render: function() {
         var selectedQuestion = this.state.selectedQuestion;
-        var qs = this.state.questions.filter(function(q) { return q != selectedQuestion; });
+        var qs = this.state.questions;//This.questions.filter(function(q) { return q != selectedQuestion; });
         return (
             <div>
-                <Question questions={this.state.questions} selectQuestion={this.selectQuestion} label="Select Root Question"/>
+                <Question questions={this.state.questions} selectQuestion={this.selectQuestion} filterFunction={this.filterQuestions} label="Select Root Question"/>
                 <Responses responses={this.state.selectedQuestion.responses} />
-                <Question questions={qs} selectQuestion={this.dummyFunction} label="Select Question to Skip"/>
+                <Question questions={qs} selectQuestion={this.dummyFunction} filterFunction={this.dummyFunction} label="Select Question to Skip"/>
             </div>
         );
     }
 });
 
-React.renderComponent(<AddSkipRule />, document.getElementById('id_create-skip-rule'));
+var component = React.renderComponent(<AddSkipRule />, document.getElementById('id_create-skip-rule'));
+
+var output= {};
+skipRules.updateSubsection = function(subsectionId) {
+    skipRules.subsection = subsectionId;
+    $.get( "/questionnaire/subsection/" + subsectionId + "/questions/", function( data ) {
+        var questions = jQuery.parseJSON( data.questions );
+        output = questions.filter(function(q) { return q.fields.answer_type == "MultiChoice"; });
+        component.setState({questions: questions});
+    }, dataType="json");
+    // var questions = [ {id: 10, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]},
+    //                     {id: 20, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]},
+    //                     {id: 30, responses: [{value:"yes", text:"yes"}, {value:"no", text:"no"}]}];  //make get request
+    // component.setState({questions: questions});
+};
