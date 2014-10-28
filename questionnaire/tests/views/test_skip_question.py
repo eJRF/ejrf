f619c9ee72bd3b4a8d347cad427073c9cd49debc
Hugo Firth <hugo.firth@gmail.com>
2014-10-28 15:16:20 +0300

    testing to make sure the response is one of the root question's options

--- questionnaire/tests/views/test_skip_question.py
+++ questionnaire/tests/views/test_skip_question.py
@@ -2,6 +2,7 @@ from questionnaire.tests.base_test import BaseTest
 from django.test import Client
 import logging
 from questionnaire.models import Question, SkipQuestion, QuestionOption, Questionnaire, Section, SubSection, QuestionGroup
+import json
 
 class SkipQuestionTest(BaseTest):
 
@@ -15,21 +16,31 @@ class SkipQuestionTest(BaseTest):
 		section = Section.objects.create(name="section", questionnaire=questionnaire, order=1)
 
 		subsection = SubSection.objects.create(title="subsection 1", section=section, order=1)
-		subsection2 = SubSection.objects.create(title="subsection 2", section=section, order=1)
+		subsection2 = SubSection.objects.create(title="subsection 2", section=section, order=1)		
+		subsection3 = SubSection.objects.create(title="subsection 2", section=section, order=1)
 		self.subsection_id = subsection.pk
-		self.empty_subsection_id = subsection2.pk
-		question_group = QuestionGroup.objects.create(subsection_id=subsection.id)
+		question_group = QuestionGroup.objects.create(subsection_id=subsection.id)	
 
-		question1 = Question.objects.create(text='Q1', UID='C00003', answer_type='MultiChoice', region=region)
-		question2 = Question.objects.create(text='Q2', UID='C00004', answer_type='Number', region=region)
+		root_question = Question.objects.create(text='Q1', UID='C00003', answer_type='MultiChoice', region=region)
+		skip_question = Question.objects.create(text='Q2', UID='C00004', answer_type='Number', region=region)
+		self.random_question = Question.objects.create(text='Q3', UID='C00005', answer_type='Number', region=region)
 		
-		question_group.question.add(question1)
-		question_group.question.add(question2)
+		question_group.question.add(root_question)
+		question_group.question.add(skip_question)
+		question_group.question.add(self.random_question)
 
-		response = QuestionOption.objects.create(text = "Some response", question=question1, UID = "U0003")
-		self.form_data = {'root-question': question1.pk,
+		self.subsection_with_only_root_question = subsection3.pk
+		question_group2 = QuestionGroup.objects.create(subsection_id=self.subsection_with_only_root_question)
+		question_group2.question.add(root_question)
+
+		self.subsection_with_only_skip_question = subsection2.pk
+		question_group3 = QuestionGroup.objects.create(subsection_id=self.subsection_with_only_skip_question)	
+		question_group3.question.add(skip_question)
+
+		response = QuestionOption.objects.create(text = "Some response", question=root_question, UID = "U0003")
+		self.form_data = {'root-question': root_question.pk,
 						  'responses': response.pk,
-						  'skip-question': question2.pk}
+						  'skip-question': skip_question.pk}
 
 	def test_post_skip_question(self):
 		self.assertEqual(SkipQuestion.objects.all().count(), 0)
@@ -42,19 +53,40 @@ class SkipQuestionTest(BaseTest):
 		data['root-question'] = 341543
 		response = self.client.post(self.url % self.subsection_id, data=data)
 		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], 'root-question does not exist')
 
 	def test_post_skip_question_for_response_not_existing(self):
 		data = self.form_data
 		data['responses'] = 341543
 		response = self.client.post(self.url % self.subsection_id, data=data)
 		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], 'response does not exist')
 
 	def test_post_skip_question_for_skip_question_not_existing(self):
 		data = self.form_data
 		data['skip-question'] = 341543
 		response = self.client.post(self.url % self.subsection_id, data=data)
 		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], 'skip-question does not exist')
 
-	def test_post_skip_question_for_questions_not_being_part_of_subsection(self):
-		response = self.client.post(self.url % self.empty_subsection_id, data=self.form_data)
+	def test_post_skip_question_for_root_question_not_being_part_of_subsection(self):
+		response = self.client.post(self.url % self.subsection_with_only_skip_question, data=self.form_data)
 		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], 'root-question is not part of subsection')
+
+	def test_post_skip_question_for_skip_question_not_being_part_of_subsection(self):
+		response = self.client.post(self.url % self.subsection_with_only_root_question, data=self.form_data)
+		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], 'skip-question is not part of subsection')
+
+	def test_post_skip_question_for_response_option_not_being_related_to_root_question(self):
+		data = self.form_data
+		data['root-question'] = self.random_question.pk
+		response = self.client.post(self.url % self.subsection_id, data=data)
+		self.assertEqual(400, response.status_code)
+		self.assertEqual(json.loads(response.content)['result'], "root question's options does not contain the provided response")
+
+
+	# def test_post_response_for_root_question(self):
+	# 	question3 = Question.objects.create(text='Q3', UID='C00005', answer_type='MultiChoice', region=region)
+	# 	
--- questionnaire/views/skip_question.py
+++ questionnaire/views/skip_question.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from questionnaire.models import SkipQuestion, Question, QuestionOption, SubSection
 import logging
+from django.core.exceptions import ValidationError
 
 class SkipQuestionView(View):
 	def error_response(self, error_message):
@@ -14,16 +15,20 @@ class SkipQuestionView(View):
 		root_question_id = 	request.POST['root-question']
 		response_id = 	request.POST['responses']	
 		skip_question_id = 	request.POST['skip-question']
+		subsection_id = kwargs['subsection_id']
 
-		try:
-			root_question = Question.objects.get(pk=root_question_id)
-			subsection = SubSection.objects.get(pk=kwargs['subsection_id'])   	
-			if root_question.question_group.filter(subsection = subsection).count() == 1:
-				SkipQuestion.objects.create(root_question=root_question, response=QuestionOption.objects.get(pk=response_id), skip_question=Question.objects.get(pk=skip_question_id))
-			else:
-				return self.error_response('Question is not part of subsection')
-		except Exception as e:
-			logging.error(e)
+		try: 	
+			
+			SkipQuestion.create(root_question_id, response_id, skip_question_id, subsection_id)
+			# else:
+				# return self.error_response('Question is not part of subsection')
+		except ValidationError as e:
+			msg = '; '.join(e.messages)
+			logging.error(msg)
+			return self.error_response(msg)
+
+		except Exception as e2:
+			logging.error(e2)
 			return self.error_response('Question or response do not exist')
 
 		data = {}
