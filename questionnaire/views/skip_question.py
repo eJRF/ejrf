01d658479c9d9dcfc2c4e1d657161bdc27e8c2e1
Hugo Firth <hugo.firth@gmail.com>
2014-10-29 11:22:48 +0300

    front end is dealing with creating skips rules and failures creating skip rules

--- questionnaire/views/skip_question.py
+++ questionnaire/views/skip_question.py
@@ -12,16 +12,16 @@ class SkipQuestionView(View):
 	def post(self, request, *args, **kwargs):
 		# print(request)
 
-		root_question_id = 	request.POST['root-question']
-		response_id = 	request.POST['responses']	
-		skip_question_id = 	request.POST['skip-question']
-		subsection_id = request.POST['subsection-id']
+		try:
+			root_question_id = 	request.POST['root-question']
+			response_id = 	request.POST['responses']	
+			skip_question_id = 	request.POST['skip-question']
+			subsection_id = request.POST['subsection-id']
+		except Exception as e:
+			return self.error_response("You must provide a root question, response, question to skip and subsection")
 
 		try: 	
-			
 			SkipQuestion.create(root_question_id, response_id, skip_question_id, subsection_id)
-			# else:
-				# return self.error_response('Question is not part of subsection')
 		except ValidationError as e:
 			msg = '; '.join(e.messages)
 			logging.error(msg)
@@ -32,5 +32,5 @@ class SkipQuestionView(View):
 			return self.error_response('Unknown error occured')
 
 		data = {}
-		data['result'] = 'success'
+		data['result'] = 'Skip rule created successfully'
 		return HttpResponse(json.dumps(data), content_type = "application/json", status=201)
