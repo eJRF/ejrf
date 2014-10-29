c056f6fbf40578ae78aed10adf96149fcc89a254
Tom Oketch <tc.oketch@gmail.com>
2014-10-29 17:23:45 +0300

    Cleaning up snap-ci directory - removing files that are no longer used

--- eJRF/snap-ci/ie-ft.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source ../ejrf_env/bin/activate
-cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
-cp eJRF/snap-ci/initial_steps.py questionnaire/features/initial_steps.py
-./manage.py harvest
--- eJRF/snap-ci/jasmine.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source ../ejrf_env/bin/activate
-pip install jasmine==2.0.0
-jasmine-ci
