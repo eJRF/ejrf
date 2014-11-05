aa4f0c81947bd83f0b0cc1d71e10a490bd4b733f
Andrew Mukiza <andrewmukiza@gmail.com>
2014-11-05 15:33:50 +0300

    Fix date format datepickers when multiple datepickers are on the page

--- questionnaire/static/bootstrap-datetimepicker/monthandyeardatepicker.js
+++ questionnaire/static/bootstrap-datetimepicker/monthandyeardatepicker.js
@@ -1,17 +1,23 @@
 $(function () {
-    var $date_option = $('.date-time-picker').data('date-option');
-    if ($date_option == "mm") {
-        $('.date-time-picker').datepicker({
-            format: "mm/yyyy",
-            startView: 1,
-            minViewMode: 1
-        });
-    } else {
-        $('.date-time-picker').datepicker({
-            format: "dd/mm/yyyy",
-            todayHighlight: true
-        });
+    var dateFields = $('.date-time-picker');
 
-    }
+    var initializeDatePicker = function (elem) {
+        var fieldData = $(elem).data('date-option');
+        if (fieldData == 'mm') {
+            $(elem).datepicker({
+                format: "mm/yyyy",
+                startView: 1,
+                minViewMode: 1
+            });
+        } else {
+            $(elem).datepicker({
+                format: "dd/mm/yyyy",
+                todayHighlight: true
+            });
+        }
+    };
 
+    dateFields.map(function (_, dtField) {
+        initializeDatePicker(dtField);
+    });
 });
