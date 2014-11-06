6cc74409ede132de4a3ec2a390bd49e6e8062ccb
Hugo Firth <hugo.firth@gmail.com>
2014-11-06 14:30:52 +0300

    fixed functional test shell script

--- eJRF/snap-ci/ft.sh
+++ eJRF/snap-ci/ft.sh
@@ -4,4 +4,4 @@ source ../ejrf_env/bin/activate
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
 echo "setup for tests done"
 ./manage.py harvest --tag=-WIP --tag=-Upload -v 2
-echo "functional test complete"
+
