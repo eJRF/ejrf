fcbcb4d3eb8e78d4e3e48b986f4d54a9f406ba77
NavaL <mnandri@thoughtworks.com>
2015-01-20 11:57:45 +0300

    [unicef_ejrf/#543] Create a display all grid: unused questionnaire filter in the questions API endpoint

--- questionnaire/api/v1/questions.py
+++ questionnaire/api/v1/questions.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.views.generic import View
 
-from questionnaire.models import Question
+from questionnaire.models import Question, Questionnaire
 
 
 class QuestionAPIView(View):
@@ -10,6 +10,20 @@ class QuestionAPIView(View):
     model = Question
 
     def get(self, *args, **kwargs):
-        data = Question.objects.all()
+        data = self._query_data()
         serialized_data = serializers.serialize('json', data)
         return HttpResponse(serialized_data, content_type="application/json")
+
+    def _query_data(self):
+        excluded_params = self._excluded_params()
+        QUESTION_FIELDS_MAPPING = {'answer_type': 'answer_type__iexact'}
+        query_params = {value: self.request.GET.get(key) for key, value in QUESTION_FIELDS_MAPPING.items() if self.request.GET.get(key)}
+        return Question.objects.filter(**query_params).exclude(**excluded_params)
+
+    def _excluded_params(self):
+        questionnaire = Questionnaire.objects.filter(id=self.request.GET.get('questionnaire'))
+        if questionnaire.exists() and self.request.GET.get('unused'):
+            questionnaire = questionnaire[0]
+            excluded_questions_ids = questionnaire.get_all_questions().values_list('id', flat=True)
+            return {'id__in': excluded_questions_ids}
+        return {}
--- questionnaire/tests/api/test_questions_api.py
+++ questionnaire/tests/api/test_questions_api.py
@@ -4,6 +4,8 @@ from django.test import Client
 
 from questionnaire.tests.base_test import BaseTest
 from questionnaire.tests.factories.question_factory import QuestionFactory
+from questionnaire.tests.factories.question_group_factory import QuestionGroupFactory
+from questionnaire.tests.factories.questionnaire_factory import QuestionnaireFactory
 from questionnaire.utils.answer_type import AnswerTypes
 
 
@@ -28,3 +30,27 @@ class QuestionAPITest(BaseTest):
         pks = [item['pk'] for item in json_response]
         self.assertIn(self.multichoice.id, pks)
         self.assertIn(self.numerical.id, pks)
+
+    def test_list_all_multichoice_questions(self):
+        response = self.client.get(self.url + '?answer_type=multichoice')
+        self.assertEqual(200, response.status_code)
+        json_response = json.loads(response.content)
+        self.assertEqual(1, len(json_response))
+
+        self.assertEqual(self.multichoice.id, json_response[0]['pk'])
+
+    def test_list_all_unused_questions_given_questionnaire(self):
+        question_group = QuestionGroupFactory()
+        question_in_questionnaire = QuestionFactory()
+        question_group.question.add(question_in_questionnaire)
+        questionnaire = question_group.subsection.section.questionnaire
+
+        response = self.client.get(self.url + '?questionnaire=%s&unused=true' % questionnaire.id)
+        self.assertEqual(200, response.status_code)
+        json_response = json.loads(response.content)
+        self.assertEqual(2, len(json_response))
+
+        pks = [item['pk'] for item in json_response]
+        self.assertIn(self.multichoice.id, pks)
+        self.assertIn(self.numerical.id, pks)
+        self.assertNotIn(question_in_questionnaire.id, pks)
