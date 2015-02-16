bcc7d87b8e8dfe6cb02de13e1d2be9874338f26b
Andrew Mukiza <andrewmukiza@gmail.com>
2015-02-16 16:44:08 +0300

    [Tech] load-fixture.sh loads from 2013-core-jrf.json

--- infrastructure/roles/app/files/load-fixtures.sh
+++ infrastructure/roles/app/files/load-fixtures.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /srv/checkout/
 source ejrfvenv/bin/activate
-python manage.py loaddata questionnaire/fixtures/questionnaire_core_2013.json
+python manage.py loaddata questionnaire/fixtures/2013-core-jrf.json
 python manage.py loaddata questionnaire/fixtures/permission.json
 python manage.py loaddata questionnaire/fixtures/organization_region_location.json
