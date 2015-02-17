8d7acfd209555d8ee2ed8868dc14ee3497e79d94
Andrew Mukiza <andrewmukiza@gmail.com>
2015-02-17 23:35:07 +0300

    add endpoint saving of question options

--- questionnaire/api/v1/question_option.py
+++ questionnaire/api/v1/question_option.py
@@ -1,7 +1,9 @@
+import json
 from django.core import serializers
 from django.http import HttpResponse
 from django.views.generic import View
-from questionnaire.models import QuestionOption
+from questionnaire.forms.questions import QuestionOptionForm
+from questionnaire.models import QuestionOption, Question
 
 
 class QuestionOptionAPIView(View):
@@ -12,3 +14,14 @@ class QuestionOptionAPIView(View):
         data = QuestionOption.objects.filter(question__id=question_id)
         serialized_data = serializers.serialize('json', data)
         return HttpResponse(serialized_data, content_type="application/json")
+
+    def post(self, *args, **kwargs):
+        question = Question.objects.get(id=kwargs['question_id'])
+        form = QuestionOptionForm(self.request.POST, question=question)
+        if form.is_valid():
+            form.save()
+            message_response = [{'message': 'The question options were reordered successfully.'}]
+            return HttpResponse(json.dumps(message_response), content_type="application/json")
+
+        message = [{'message': 'The question options could not be reordered successfully.'}]
+        return HttpResponse(json.dumps(message), content_type="application/json", status=400)
--- questionnaire/tests/api/test_question_options.py
+++ questionnaire/tests/api/test_question_options.py
@@ -10,9 +10,9 @@ from questionnaire.tests.factories.question_option_factory import QuestionOption
 class ThemesAPITest(BaseTest):
     def setUp(self):
         self.client = Client()
-        # self.user = self.create_user(group=self.GLOBAL_ADMIN, org="WHO")
-        # self.assign('can_edit_questionnaire', self.user)
-        # self.client.login(username=self.user.username, password='pass')
+        self.user = self.create_user(group=self.GLOBAL_ADMIN, org="WHO")
+        self.assign('can_edit_questionnaire', self.user)
+        self.client.login(username=self.user.username, password='pass')
 
         self.question = QuestionFactory()
         self.question_option1 = QuestionOptionFactory(question=self.question)
@@ -20,7 +20,7 @@ class ThemesAPITest(BaseTest):
         self.question_option3 = QuestionOptionFactory(question=self.question)
         self.url = '/api/v1/question/%s/options/' % self.question.id
 
-    def test_list_themes(self):
+    def test_list_options(self):
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
         json_response = json.loads(response.content)
@@ -30,3 +30,18 @@ class ThemesAPITest(BaseTest):
         self.assertIn(self.question_option1.id, pks)
         self.assertIn(self.question_option2.id, pks)
         self.assertIn(self.question_option3.id, pks)
+
+    def test_post_re_order_options_with_valid_form(self):
+        response = self.client.post(self.url, {'options': [self.question_option1.id, self.question_option3.id, self.question_option2.id]})
+        self.assertEqual(200, response.status_code)
+        json_response = json.loads(response.content)[0]
+        self.assertEqual(1, len(json_response))
+        self.assertEqual('The question options were reordered successfully.', json_response['message'])
+
+    def test_post_re_order_options_with_invalid_form(self):
+        un_known_option = 33
+        response = self.client.post(self.url, {'options': [self.question_option1.id, self.question_option3.id, un_known_option]})
+        self.assertEqual(400, response.status_code)
+        json_response = json.loads(response.content)[0]
+        self.assertEqual(1, len(json_response))
+        self.assertEqual('The question options could not be reordered successfully.', json_response['message'])
